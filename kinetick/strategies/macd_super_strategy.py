5474a13c63cdf1b25dfd3bda62f46b84118d7b0e
vindows18 <vindows18@gmail.com>
2021-08-26 13:43:07 +0530

    add LICENSE

--- kinetick/strategies/macd_super_strategy.py
+++ kinetick/strategies/macd_super_strategy.py
@@ -1,16 +1,10 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# QTPyLib: Quantitative Trading Python Library
-# https://github.com/ranaroussi/qtpylib
-#
-# Copyright 2016-2018 Ran Aroussi
+# Copyright [2021] [vin8tech]
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
