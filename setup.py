0171350f70cd81c0ef55ae13d2bcd6cf5727b53c
vindows18 <vindows18@gmail.com>
2021-09-02 17:49:37 +0530

    bump up version to 1.0.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='kinetick',
-    version='1.0.1',
+    version='1.0.2',
     description='Simplifying Trading',
     long_description=long_description,
     url='https://github.com/imvinaypatil/kinetick',
