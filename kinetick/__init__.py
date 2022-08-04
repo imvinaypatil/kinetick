540b00a159a913c7c76214e994f934946bf314eb
Vinay <vinaygouda@plumhq.com>
2022-08-04 23:01:45 +0530

    add talib.STOCHRSIS

--- kinetick/__init__.py
+++ kinetick/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-__version__ = '1.0.8rc2'
+__version__ = '1.0.8rc3'
 __author__ = 'vin8tech'
 
 import os
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='kinetick',
-    version='1.0.8rc2',
+    version='1.0.8rc3',
     description='Simplifying Trading',
     long_description=long_description,
     url='https://github.com/imvinaypatil/kinetick',
